c84c0ea6b2ff18f1524865c7bb1ab57e9b5715c0
kishor reddy <108381452+kishor12reddy@users.noreply.github.com>
2024-10-16 11:12:17 +0530

    Update train_mask_detector.py

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -35,13 +35,17 @@ labels = []
 for category in CATEGORIES:
     path = os.path.join(DIRECTORY, category)
     for img in os.listdir(path):
-    	img_path = os.path.join(path, img)
-    	image = load_img(img_path, target_size=(224, 224))
-    	image = img_to_array(image)
-    	image = preprocess_input(image)
+        img_path = os.path.join(path, img)
+        try:
+            image = load_img(img_path, target_size=(224, 224))
+            image = img_to_array(image)
+            image = preprocess_input(image)
 
-    	data.append(image)
-    	labels.append(category)
+            data.append(image)
+            labels.append(category)
+        except Exception as e:
+            print(f"Error loading image {img_path}: {e}")
+
 
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
